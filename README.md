82e3e4fc7f80052a795dace6f7eabdf3105285e5
Siva Rama Krishna <situmma@cisco.com>
2020-02-07 17:33:53 +0530

    Added Express Readme file

---
+++ README.md
@@ -0,0 +1,12 @@
+## NodeJS File Upload Example
+
+# Run below commands to start the applicion
+
+```
+
+npm install
+npm start
+
+Open localhost:3000 in browser.
+
+```
--- server.js
+++ server.js
@@ -1,31 +1,26 @@
-var http = require("http");
-var formidable = require("formidable");
-var fs = require("fs");
-var path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-http
-  .createServer(function(req, res) {
-    if (req.url == "/fileupload") {
-      var form = new formidable.IncomingForm();
-      form.parse(req, function(err, fields, files) {
-        var oldpath = files.filetoupload.path;
-        var newpath =
-          path.resolve(__dirname, "documents") + "/" + files.filetoupload.name;
-        fs.rename(oldpath, newpath, function(err) {
-          if (err) throw err;
-          res.write("File uploaded and moved!");
-          res.end();
-        });
-      });
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(
-        '<form action="fileupload" method="post" enctype="multipart/form-data">'
-      );
-      res.write('<input type="file" name="filetoupload"><br>');
-      res.write('<input type="submit">');
-      res.write("</form>");
-      return res.end();
-    }
-  })
-  .listen(8080);
+const formidable = require("formidable");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+
+app.use(cors());
+app.options("*", cors());
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/index.html")));
+app.post("/fileupload", (req, res) => {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    var oldpath = files.filetoupload.path;
+    var newpath =
+      path.resolve(__dirname, "documents") + "/" + files.filetoupload.name;
+    fs.rename(oldpath, newpath, function(err) {
+      if (err) throw err;
+      res.send("File uploaded and moved!");
+    });
+  });
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
